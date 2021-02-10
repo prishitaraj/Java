73f04f6dd6a4738cca9d5bd5e4ff663cba2b60ed
prishitaraj <59114909+prishitaraj@users.noreply.github.com>
2021-02-10 21:45:45 +0530

    Add files via upload
    Programme 1

---
+++ Main.java
@@ -0,0 +1,17 @@
+package com.company;
+
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+        int c = scan.nextInt();
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+    }
+}
