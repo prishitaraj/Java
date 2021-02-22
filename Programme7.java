b7ebeb214a40b7cb0e47c2a321a255bf8b8f6790
Prishita Raj <59114909+prishitaraj@users.noreply.github.com>
2021-02-22 15:23:11 +0530

    Add files via upload

---
+++ Programme7.java
@@ -0,0 +1,45 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+import java.util.*;
+import java.io.*;
+
+
+
+class Main{
+    public static void main(String []argh)
+    {
+
+
+
+        Scanner sc = new Scanner(System.in);
+        int t=sc.nextInt();
+
+        for(int i=0;i<t;i++)
+        {
+
+            try
+            {
+                long x=sc.nextLong();
+                System.out.println(x+" can be fitted in:");
+                if(x>=-128 && x<=127)System.out.println("* byte");
+                if(x>=-32768 && x<=32767)System.out.println("* short");
+                if(x>=Math.pow(-2,31) && x<=Math.pow(2,31)-1)System.out.println("* int");
+                if(x>=Math.pow(-2,63) && x<=Math.pow(2,63)-1)System.out.println("* long");
+            }
+            catch(Exception e)
+            {
+                System.out.println(sc.next()+" can't be fitted anywhere.");
+            }
+
+        }
+    }
+}
+
+
+
