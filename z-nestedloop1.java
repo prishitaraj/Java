cf9b6ec60472e2cb0e6c008a442e2ef6521c3aaa
Prishita Raj <59114909+prishitaraj@users.noreply.github.com>
2021-03-27 23:27:31 +0530

    Updated

--- z-nestedloop1.java
+++ z-nestedloop1.java
@@ -6,7 +6,7 @@ C#, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, S
 Code, Compile, Run and Debug online from anywhere in world.
 
 *******************************************************************************/
-public class Main
+public class z-nestedloop1
 {
 	public static void main(String[] args) {
 		for(int i=1;i<=6;i++)
