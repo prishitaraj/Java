3c039f0cc8c152dcc9cfb3ce9fd28fb83ad8162d
Prishita Raj <59114909+prishitaraj@users.noreply.github.com>
2021-03-28 21:39:40 +0530

    Add files via upload

---
+++ z-nestedloop2.java
@@ -0,0 +1,24 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+import java.util.Scanner;
+public class z-nestedloop2
+{
+	public static void main(String[] args) {
+		for(int i=65;i<=67;i++)
+		{
+		    for(int j=65;j<=i;j++)
+		   {
+		        System.out.print((char)j+" ");
+		   }
+		   System.out.println();
+		}
+	}
+}
+
+
